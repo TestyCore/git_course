18fb95ab51094ceaf1685a26af446fa452bf271e
Vyacheslav Tatarinov <tatarinov.slava222@gmail.com>
2022-09-09 10:15:49 +0300

    chenges .cpp

--- git_training.cpp
+++ git_training.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello Wodrld!\n";
+    void new_line(int* ptr);
+
+    int* line = new int[4] {1, 2, 3, 4};
+    cout << endl << &line;
+    new_line(line);
+    cout << line[0];
+    //std::cout << "Hello Wodrld!\n";
 }
 
-void new_ine() {
-
+void new_line(int* ptr) {
+    cout << endl << &ptr;
+    ptr[0] = 99;
 }
